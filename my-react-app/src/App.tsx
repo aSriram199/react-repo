57fd35eaf667af3abdac3da11e19797f1473b72b
siuuu09 <157874007+siuuu09@users.noreply.github.com>
2024-01-27 16:46:03 +0530

    Update App.tsx

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -8,6 +8,7 @@ const CounterApp = () => {
       <h1>Counter: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
+       <button onClick={setCount((count = 0))}>Reset</button>
     </div>
   );
 };
